5bf8a5ec3c974675bd543834a1292e5f7dff0d3c
vivianlsong <63174230+vivianlsong@users.noreply.github.com>
2020-07-11 01:57:06 -0700

    profile api and validation added

---
+++ models/Profile.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  bio: {
+    type: String
+  },
+  location: {
+      type: String
+  },
+  email: {
+    type: String
+  },
+  website: {
+    type: String
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
---
+++ validation/profile.js
@@ -0,0 +1,27 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to between 2 and 40 characters";
+  }
+
+  if (isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required";
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
