cca33c07c38bb32b6945b13ebcd68b9f649511c1
vivianlsong <63174230+vivianlsong@users.noreply.github.com>
2020-07-26 02:11:45 -0700

    route & functionality added to register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className = "App">
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className = "App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
